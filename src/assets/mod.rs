4a5a0014165ef5de8df5f7f07c51be0c1e7dff83
SecretPocketCat <jiridexterhanc@gmail.com>
2023-03-26 17:29:01 +0200

    fix progress tracking

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -1,30 +1,36 @@
-use std::ops::RangeInclusive;
+use std::{ops::RangeInclusive, time::Duration};
 
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use iyes_progress::{ProgressCounter, ProgressPlugin};
 
-use crate::AppState;
+use crate::{time::time::after_delay, AppState};
 
 pub mod audio;
 pub mod fonts;
+mod splash;
 pub mod textures;
 mod window_icon;
 
-use self::window_icon::set_window_icon;
+use self::{splash::print_progress, window_icon::set_window_icon};
 
 pub fn assets_plugin(app: &mut App) {
     app.add_startup_system(set_window_icon)
         .add_loading_state(LoadingState::new(AppState::Splash).continue_to_state(AppState::Loading))
-        .add_collection_to_loading_state::<_, textures::SplashScreenTextureAssets>(AppState::Splash)
+        .add_collection_to_loading_state::<_, splash::SplashScreenTextureAssets>(AppState::Splash)
         .add_loading_state(LoadingState::new(AppState::Loading))
         .add_collection_to_loading_state::<_, fonts::FontAssets>(AppState::Loading)
         .add_collection_to_loading_state::<_, audio::SfxAssets>(AppState::Loading)
         .add_collection_to_loading_state::<_, audio::AudioAssets>(AppState::Loading)
         .add_collection_to_loading_state::<_, textures::TextureAssets>(AppState::Loading)
         .add_startup_system(audio::setup_sfx_assets)
+        .add_plugin(ProgressPlugin::new(AppState::Splash))
         .add_plugin(ProgressPlugin::new(AppState::Loading))
-        .add_system(print_progress.in_set(OnUpdate(AppState::Loading)));
+        .add_system(
+            print_progress
+                .in_set(OnUpdate(AppState::Loading))
+                .run_if(after_delay(Duration::from_secs(3))),
+        );
 }
 
 fn add_dynamic_assets(
@@ -42,14 +48,4 @@ fn add_dynamic_assets(
                 .collect(),
         }),
     );
-}
-
-fn print_progress(progress: Res<ProgressCounter>, mut last_done: Local<u32>) {
-    let progress = progress.progress();
-    warn!("Progress: {:?}", progress);
-
-    if progress.done > *last_done {
-        *last_done = progress.done;
-        warn!("Changed progress: {:?}", progress);
-    }
 }
